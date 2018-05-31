9fb8b2f6f76e14e70a21ee6a0c16946ac93861ae
sudoferraz <jedygabriel@gmail.com>
2018-05-31 15:51:54 -0300

    Added create match

--- bets/migrations/1_initial_migration.js
+++ bets/migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
 var Migrations = artifacts.require("./BetsBase.sol");
+var POE = artifacts.require("./ProofOfExistence1.sol");
+
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(POE);
 };
